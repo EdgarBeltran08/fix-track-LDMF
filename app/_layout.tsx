f7bf6808854c748037abd49f047e4e3a838a4c4b
Omar Reyes L. <relo2210@gmail.com>
2025-09-07 00:08:44 -0600

    1. Layout branch has been made, it inherits SafeAreaInsets (Not SafeAreaView because of its Deprecation) to all .tsx files. 2. Also stays the Status Bar balanced automatically. 3. Stack Structure for auth & tabBar mechaincs.
    Note: It's needed to test the Stack if its workable.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,32 @@
-import { Stack } from "expo-router";
-
 import { GluestackUIProvider } from "@/shared/components/ui/gluestack-ui-provider";
 import "@/shared/styles/globals.css";
+import { Stack } from "expo-router";
+import { View } from "react-native";
+import { SafeAreaProvider, useSafeAreaInsets } from "react-native-safe-area-context";
+
+function LayoutContainer({ children }: { children: React.ReactNode }) {
+  const insets = useSafeAreaInsets();
+  return (
+    <View style={{ flex: 1, paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      {children}
+    </View>
+  );
+}
 
 export default function RootLayout() {
   return (
     <GluestackUIProvider mode="system">
-      <Stack />
+      <SafeAreaProvider>
+        <LayoutContainer>
+
+          <Stack>
+            <Stack.Screen name="auth" options={{ headerShown: false }}/>
+
+            <Stack.Screen name="(tab)" options={{ headerShown: false }}/>
+          </Stack>
+
+        </LayoutContainer>
+      </SafeAreaProvider>
     </GluestackUIProvider>
   );
 }
