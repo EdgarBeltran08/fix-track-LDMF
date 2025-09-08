e6be297353f1efe8e6faa53070790dc3909975d3
Leon Casas <leonn.casass@gmail.com>
2025-09-08 00:27:50 -0600

    feat: implement authentication layout and form controls for user login

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,11 @@
-import {
-  Button,
-  ButtonSpinner,
-  ButtonText,
-} from "@/shared/components/ui/button";
-import { Text, View } from "react-native";
-import "../shared/styles/globals.css";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View className="flex-1 items-center justify-center bg-background-50">
-      <Text className="text-xl font-bold text-primary-500">
-        Bienvenido a FixTrack
-      </Text>
-      <Button className="p-3">
-        <ButtonSpinner color="gray" />
-        <ButtonText className="font-medium text-sm ml-2">
-          Start to develop!...
-        </ButtonText>
-      </Button>
-    </View>
-  );
+  const isAuthenticated = false; // Replace with your actual authentication logic
+
+  if (isAuthenticated) {
+    return <Redirect href="/(private)/(tabs)" />;
+  }
+
+  return <Redirect href="/(auth)" />;
 }
