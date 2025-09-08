8aaef497d8cb2963b9b1c8fe18bf301adb82c7cd
Edgar Beltran <beltranyuyu05@gmail.com>
2025-09-07 22:43:19 -0600

    Cambié etiquetas para el uso de la paleta de colores establecida

--- app/inventory_ui/index.tsx
+++ app/inventory_ui/index.tsx
@@ -1,6 +1,6 @@
 import { Button, ButtonText } from '@/shared/components/ui/button';
 import { Input, InputField, InputSlot } from '@/shared/components/ui/input';
-import { Modal, ModalBackdrop, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader } from '@/shared/components/ui/modal'; // Importar el Modal de Gluestack
+import { Modal, ModalBackdrop, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader } from '@/shared/components/ui/modal';
 import { Table, TableBody, TableData, TableFooter, TableHead, TableHeader, TableRow } from '@/shared/components/ui/table';
 import React, { useState } from 'react';
 import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
@@ -25,50 +25,38 @@ const InventoryPage: React.FC = () => {
     item.repuesto.toLowerCase().includes(searchText.toLowerCase())
   );
 
-
   const totalItems = filteredData.reduce((sum, item) => sum + item.cantidad, 0);
 
-  
   const handleClickVer = (item: any) => {
     setSelectedItem(item); 
     setIsModalOpen(true);   
   };
 
-  
   const handleCloseModal = () => {
     setIsModalOpen(false); 
   };
 
   return (
-    <View style={{ flex: 1, paddingTop: 20, marginBottom: 30, alignItems: 'flex-start' }}>
-      <Text style={{ fontSize: 30, fontWeight: 'bold', marginLeft: 10 }}>Inventario</Text>
+    <View className="flex-1 pt-5 mb-8 items-start bg-background-50">
+      <Text className="text-3xl font-bold ml-6 text-secondary-900">Inventario</Text>
 
       {/* Barra de busqueda */}
-      <View style={{ marginVertical: 10, width: '55%', marginLeft: 10 }}>
-        <Input style={{ borderWidth: 1.5, borderRadius: 10, borderColor: '#000000' }}>
-          <InputSlot className="pl-3">
-          </InputSlot>
+      <View className="my-2 w-3/5 ml-5">
+        <Input className="border-2 border-secondary-300 rounded-xl">
+          <InputSlot className="pl-3"></InputSlot>
           <InputField 
             placeholder="Buscar Item..." 
             value={searchText}
             onChangeText={setSearchText}
-            style={{ color: 'black', fontWeight: 'bold' }}  
+            className="text-black font-bold"  
           />
         </Input>
       </View>
 
-      
-      <View style={{
-        flex: 1, 
-        paddingTop: 10, 
-        marginHorizontal: 10, 
-        width: '95%', 
-        maxHeight: 550,  
-        borderBottomWidth: 2,  
-        borderRadius: 8,  
-      }}>
+      {/* Contenedor de la tabla */}
+      <View className="flex-1 pt-2 ml-5 mx-2 w-11/12 max-h-[550px] border-b-2 border-primary rounded-xl">
         <ScrollView 
-          style={{ flex: 1 }} 
+          className="flex-1"
           showsVerticalScrollIndicator={true}
           showsHorizontalScrollIndicator={true}
           nestedScrollEnabled={true}
@@ -76,65 +64,46 @@ const InventoryPage: React.FC = () => {
           <ScrollView 
             horizontal={true}
             showsHorizontalScrollIndicator={true}
-            style={{ width: '100%' }}
+            className="w-full"
           >
-            <Table style={{
-              minWidth: 600, 
-              borderWidth: 2, 
-              borderColor: '#000000ff',  
-              borderRadius: 10,       
-              overflow: 'visible'     
-            }}>
+            <Table className="min-w-[600px] border-2 border-secondary-300 rounded-xl overflow-visible">
               <TableHeader>
-                <TableRow className="bg-primary-200">
-                  <TableHead className="text-primary-900" style={{ minWidth: 110, paddingHorizontal: 8 }}>Repuesto</TableHead>
-                  <TableHead className="text-primary-900" style={{ minWidth: 90, paddingHorizontal: 8 }}>Cantidad</TableHead>
-                  <TableHead className="text-primary-900" style={{ minWidth: 70, paddingHorizontal: 8 }}>ID</TableHead>
-                  <TableHead className="text-primary-900" style={{ minWidth: 100, paddingHorizontal: 8 }}>Categoría</TableHead>
-                  <TableHead className="text-primary-900" style={{ minWidth: 100, paddingHorizontal: 8 }}>Estado</TableHead>
-                  <TableHead className="text-primary-900" style={{ minWidth: 100, paddingHorizontal: 8 }}>Detalle</TableHead>
+                <TableRow className="bg-background-50">
+                  <TableHead className="text-secondary-900 px-2">Repuesto</TableHead>
+                  <TableHead className="text-secondary-900 px-2">Cantidad</TableHead>
+                  <TableHead className="text-secondary-900 px-2">ID</TableHead>
+                  <TableHead className="text-secondary-900 px-2">Categoría</TableHead>
+                  <TableHead className="text-secondary-900 px-2">Estado</TableHead>
+                  <TableHead className="text-secondary-900 px-2">Detalle</TableHead>
                 </TableRow>
               </TableHeader>
 
               <TableBody>
                 {filteredData.map((item) => (
-                  <TableRow key={item.id} style={{ backgroundColor: '#e0e0e0ff' }}>
-                    <TableData style={{ minWidth: 120, paddingHorizontal: 8, color: '#000000', borderBottomWidth: 1, borderBottomColor: '#000000ff' }}>
-                      {item.repuesto}
-                    </TableData>
-                    <TableData style={{ minWidth: 80, paddingHorizontal: 8, color: '#000000', borderBottomWidth: 1, borderBottomColor: '#000000ff' }}>
-                      {item.cantidad}
-                    </TableData>
-                    <TableData style={{ minWidth: 80, paddingHorizontal: 8, color: '#000000', borderBottomWidth: 1, borderBottomColor: '#000000ff' }}>
-                      {item.idRepuesto}
-                    </TableData>
-                    <TableData style={{ minWidth: 100, paddingHorizontal: 8, color: '#000000', borderBottomWidth: 1, borderBottomColor: '#000000ff' }}>
-                      {item.categoria}
-                    </TableData>
-                    <TableData style={{ minWidth: 100, paddingHorizontal: 8, color: '#000000', borderBottomWidth: 1, borderBottomColor: '#000000ff' }}>
-                      {item.estado}
-                    </TableData>
-                    <TableData style={{ minWidth: 100, paddingHorizontal: 8, color: '#000000', borderBottomWidth: 1, borderBottomColor: '#000000ff' }}>
-                      
-
-
+                  <TableRow key={item.id} className="bg-background-50">
+                    <TableData className="px-2 text-secondary-900  border-secondary-300">{item.repuesto}</TableData>
+                    <TableData className="px-2 text-secondary-900  border-secondary-300">{item.cantidad}</TableData>
+                    <TableData className="px-2 text-secondary-900  border-secondary-300">{item.idRepuesto}</TableData>
+                    <TableData className="px-2 text-secondary-900 border-secondary-300">{item.categoria}</TableData>
+                    <TableData className="px-2 text-secondary-900  border-secondary-300">{item.estado}</TableData>
+                    <TableData className="px-2 text-secondary-900  border-secondary-300">
+                      {/* Hacer clickeable el texto "Ver" */}
                       <TouchableOpacity onPress={() => handleClickVer(item)}>
-                        <Text style={{ marginLeft: 25, color: '#0066cc', textDecorationLine: 'underline', fontSize: 15 , fontWeight: 'bold' }}>Ver</Text> 
+                        <Text className="ml-6 text-blue-600 underline text-base font-bold">Ver</Text> 
                       </TouchableOpacity>
                     </TableData>
                   </TableRow>
                 ))}
               </TableBody>
 
-
               <TableFooter>
                 <TableRow className="bg-secondary-200">
-                  <TableHead className="text-secondary-900" style={{ minWidth: 120, paddingHorizontal: 8 }}>Total Items</TableHead>
-                  <TableHead className="text-secondary-900" style={{ minWidth: 80, paddingHorizontal: 8 }}>{totalItems}</TableHead>
-                  <TableHead className="text-secondary-900" style={{ minWidth: 80, paddingHorizontal: 8 }}>-</TableHead>
-                  <TableHead className="text-secondary-900" style={{ minWidth: 100, paddingHorizontal: 8 }}>-</TableHead>
-                  <TableHead className="text-secondary-900" style={{ minWidth: 100, paddingHorizontal: 8 }}>-</TableHead>
-                  <TableHead className="text-secondary-900" style={{ minWidth: 100, paddingHorizontal: 8 }}>-</TableHead>
+                  <TableHead className="text-secondary-900 px-2">Total Items</TableHead>
+                  <TableHead className="text-secondary-900 px-2">{totalItems}</TableHead>
+                  <TableHead className="text-secondary-900 px-2">-</TableHead>
+                  <TableHead className="text-secondary-900 px-2">-</TableHead>
+                  <TableHead className="text-secondary-900 px-2">-</TableHead>
+                  <TableHead className="text-secondary-900 px-2">-</TableHead>
                 </TableRow>
               </TableFooter>
             </Table>
@@ -142,7 +111,7 @@ const InventoryPage: React.FC = () => {
         </ScrollView>
       </View>
 
-      
+      {/* Modal */}
       <Modal
         isOpen={isModalOpen}
         onClose={handleCloseModal}
@@ -151,7 +120,7 @@ const InventoryPage: React.FC = () => {
         <ModalBackdrop />
         <ModalContent>
           <ModalHeader>
-            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Detalles del Repuesto</Text>
+            <Text className="text-xl font-bold">Detalles del Repuesto</Text>
             <ModalCloseButton />
           </ModalHeader>
           <ModalBody>
