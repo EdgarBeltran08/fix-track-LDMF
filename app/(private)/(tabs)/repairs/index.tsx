cbe508922314fbe26b6c1f8eac16006904a080c6
Leon Casas <leonn.casass@gmail.com>
2025-09-20 18:08:02 -0600

    feat: Add sign out button to RepairsScreen and include logo SVG asset

--- app/(private)/(tabs)/repairs/index.tsx
+++ app/(private)/(tabs)/repairs/index.tsx
@@ -1,9 +1,14 @@
+import { useUserStore } from "@/shared/stores/useUserStore";
+import { Button } from "@react-navigation/elements";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function RepairsScreen() {
+  const { signOut } = useUserStore();
+
   return (
     <View style={styles.container}>
       <Text>Repairs Screen</Text>
+      <Button onPress={() => signOut()}>Press Me</Button>
     </View>
   );
 }
