83f50ddc32240d66be4fe928036da84a63b5cbc7
Fabianbr7 <fabianalbertobenavides@gmail.com>
2025-10-13 15:16:58 -0600

    Agregué un modal en la interfaz de inventory en el archivo index, el mod… …al es para agregar un objeto al inventario, tambien instale react-native-picker/picker. Kimberley López.

--- app/(private)/(tabs)/inventory/index.tsx
+++ app/(private)/(tabs)/inventory/index.tsx
@@ -1,3 +1,4 @@
+@ -1,21 +1,25 @@
 import { Button, ButtonText } from "@/shared/components/ui/button";
 import { Input, InputField, InputSlot } from "@/shared/components/ui/input";
 import {
@@ -15,142 +16,31 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 import Feather from "@expo/vector-icons/Feather";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import React, { useEffect, useState } from "react";
+//impor de modal
+import { Picker } from "@react-native-picker/picker";
 import {
+  KeyboardAvoidingView,
+  Platform,
   Modal as RNModal,
   RefreshControl,
   ScrollView,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-
-const InventoryPage: React.FC = () => {
-  const [searchText, setSearchText] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedItem, setSelectedItem] = useState<InventoryItem | null>(null);
-
-  const [isFilterMenuOpen, setIsFilterMenuOpen] = useState(false);
-
-  const [selectedCategory, setSelectedCategory] = useState<string>("");
+@ -35,6 +39,7 @@ const InventoryPage: React.FC = () => {
 
   const [inventoryItems, setInventoryItems] = useState<InventoryItem[]>([]);
   const [refreshing, setRefreshing] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
 
   const categories = [
     "",
-    "Pantallas",
-    "Baterias",
-    "Conectores",
-    "Placas Bases",
-    "Cámaras",
-    "Micas",
-  ];
-
-  const filteredData = inventoryItems.filter(
-    (item) =>
-      item.name.toLowerCase().includes(searchText.toLowerCase()) &&
-      (selectedCategory === "" || item.category?.name === selectedCategory)
-  );
-
-  const totalItems = filteredData.length;
-
-  const handleClickVer = (item: InventoryItem) => {
-    setSelectedItem(item);
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const toggleFilterMenu = () => {
-    setIsFilterMenuOpen(!isFilterMenuOpen);
-  };
-
-  const handleSelectCategory = (category: string) => {
-    setSelectedCategory(category);
-    setIsFilterMenuOpen(false);
-  };
-
-  const fetchInventoryItems = async () => {
-    try {
-      const items = await InventoryRepository.getAll();
-      setInventoryItems(items);
-    } catch (error) {
-      console.error("Error fetching inventory items:", error);
-    }
-  };
-
-  const onRefresh = async () => {
-    setRefreshing(true);
-    await fetchInventoryItems();
-    setRefreshing(false);
-  };
-
+@ -90,6 +95,7 @@ const InventoryPage: React.FC = () => {
   useEffect(() => {
     fetchInventoryItems();
   }, []);
+  const [newCategory, setNewCategory] = useState("");
 
   return (
     <>
-      <View className="flex-1 pt-5 mb-8 items-start bg-background-50">
-        <Text className="text-3xl font-bold ml-6 mb-2 text-secondary-900">
-          Inventory
-        </Text>
-        <Text className="text-xl ml-6 mb-2 text-primary-500">
-          Control de Inventario
-        </Text>
-
-        {/* Barra de busqueda */}
-        <View className="my-2 w-full flex flex-row items-center mb-3 ml-5">
-          <Input className="bg-background-50 rounded-xl flex-row items-center border-2 border-primary-300 w-64 mr-6">
-            <InputSlot className="pl-3">
-              <AntDesign name="search" size={24} color="gray" />
-            </InputSlot>
-            <InputField
-              placeholder="Search"
-              value={searchText}
-              onChangeText={setSearchText}
-              className="text-secondary-900 font-bold bg-background-50"
-            />
-          </Input>
-
-          <View className="flex flex-row space-x-2 ml-10 mr-5 relative">
-            <TouchableOpacity
-              onPress={toggleFilterMenu}
-              className="rounded-full p-2 bg-background-300 flex-row justify-center items-center h-11"
-            >
-              <Feather
-                name="filter"
-                size={24}
-                color="white"
-                className="align-middle translate-y-[1px]"
-              />
-            </TouchableOpacity>
-
-            {isFilterMenuOpen && (
-              <View className="absolute top-14 right-0 bg-background-100 border-2 border-primary-300 rounded-md shadow-lg z-50 w-40">
-                <Text className="px-4 py-2 font-bold text-secondary-900 border-b border-primary-300">
-                  Categoría:
-                </Text>
-                {categories.map((cat) => (
-                  <TouchableOpacity
-                    key={cat}
-                    onPress={() => handleSelectCategory(cat)}
-                    className={`px-4 py-2 ${
-                      selectedCategory === cat ? "bg-primary-300" : ""
-                    }`}
-                  >
-                    <Text
-                      className={
-                        selectedCategory === cat
-                          ? "text-primary-900"
-                          : "text-primary-900"
-                      }
-                    >
-                      {cat === "" ? "Todas" : cat}
-                    </Text>
-                  </TouchableOpacity>
+@ -154,10 +160,11 @@ const InventoryPage: React.FC = () => {
                 ))}
               </View>
             )}
@@ -158,187 +48,120 @@ const InventoryPage: React.FC = () => {
             <Button
               size="lg"
               className="rounded-full ml-4 p-2 bg-primary-400 border-2 border-primary-400 flex-row justify-center items-center h-11"
+              //ON PRESS PARA MODAL
+              onPress={() => setIsAddModalOpen(true)}
             >
               <FontAwesome6
                 name="add"
-                size={24}
-                color="black"
-                className="align-middle translate-y-[-2px]"
-              />
-            </Button>
+@ -337,6 +344,111 @@ const InventoryPage: React.FC = () => {
           </View>
         </View>
-
-        {/* Contenedor de la tabla */}
-        <View className="flex-1 rounded-lg pt-2 ml-5 mx-2 w-11/12 max-h-[500px]">
-          <ScrollView
-            className="flex-1"
-            showsVerticalScrollIndicator={true}
-            showsHorizontalScrollIndicator={true}
-            nestedScrollEnabled={true}
-            refreshControl={
-              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-            }
-          >
-            <ScrollView
-              horizontal={true}
-              showsHorizontalScrollIndicator={true}
-              className="w-full"
-            >
-              <Table className="min-w-[600px] border-primary-400 rounded-lg overflow-visible">
-                <TableHeader>
-                  <TableRow className="bg-background-100 border-b-2 border-primary-400">
-                    <TableHead className="text-secondary-900 text-center px-5 py-3 text-lg">
-                      Repuesto
-                    </TableHead>
-                    <TableHead className="text-secondary-900 text-center px-5 py-3 text-lg">
-                      Costo Unitario
-                    </TableHead>
-                    <TableHead className="text-secondary-900 text-center px-5 py-3 text-lg">
-                      SKU
-                    </TableHead>
-                    <TableHead className="text-secondary-900 text-center px-5 py-3 text-lg">
-                      Categoría
-                    </TableHead>
-                    <TableHead className="text-secondary-900 text-center px-5 py-3 text-lg">
-                      Estado
-                    </TableHead>
-                    <TableHead className="text-secondary-900 text-center px-5 py-3 text-lg">
-                      Detalle
-                    </TableHead>
-                  </TableRow>
-                </TableHeader>
-
-                <TableBody>
-                  {filteredData.map((item) => (
-                    <TableRow key={item.id} className="bg-background-100">
-                      <TableData className="px-5 py-3 text-secondary-900 text-center border-b-1 border-secondary-300 text-md">
-                        {item.name}
-                      </TableData>
-                      <TableData className="px-5 py-3 text-secondary-900 text-center border-b-1 border-secondary-300 text-md">
-                        ${item.unitCost}
-                      </TableData>
-                      <TableData className="px-5 py-3 text-secondary-900 text-center border-b-1 border-secondary-300 text-md">
-                        {item.sku || "N/A"}
-                      </TableData>
-                      <TableData className="px-5 py-3 text-secondary-900 text-center border-b-1 border-secondary-300 text-md">
-                        {item.category?.name || "N/A"}
-                      </TableData>
-                      <TableData className="px-5 py-3 text-secondary-900 text-center border-b-1 border-secondary-300 text-md">
-                        {item.state === "available"
-                          ? "Disponible"
-                          : "No Disponible"}
-                      </TableData>
-                      <TableData className="px-5 py-3 text-secondary-900 text-center border-b-1 border-secondary-300 text-md flex justify-center items-center">
-                        <Button
-                          variant="link"
-                          onPress={() => handleClickVer(item)}
-                          className="p-0 mx-auto"
-                        >
-                          <AntDesign
-                            name="eye"
-                            size={24}
-                            color="#3ed389ff"
-                            className="ml-10"
-                          />
-                        </Button>
-                      </TableData>
-                    </TableRow>
-                  ))}
-                </TableBody>
-
-                <TableFooter>
-                  <TableRow className="bg-secondary-200">
-                    <TableHead className="text-secondary-900 px-5 py-5 text-md">
-                      Total Items
-                    </TableHead>
-                    <TableHead className="text-secondary-900 px-5 py-5 text-md">
-                      {totalItems}
-                    </TableHead>
-                    <TableHead className="text-secondary-900 px-5 py-5 text-md">
-                      -
-                    </TableHead>
-                    <TableHead className="text-secondary-900 px-5 py-5 text-md">
-                      -
-                    </TableHead>
-                    <TableHead className="text-secondary-900 px-5 py-5 text-md">
-                      -
-                    </TableHead>
-                    <TableHead className="text-secondary-900 px-5 py-5 text-md">
-                      -
-                    </TableHead>
-                  </TableRow>
-                </TableFooter>
-              </Table>
-            </ScrollView>
-          </ScrollView>
-        </View>
-      </View>
+      </RNModal>
 
       <RNModal
-        visible={isModalOpen}
+        //MODAL PARA AGREGAR OBJETO AL INVENTARIO
+        visible={isAddModalOpen}
         animationType="slide"
         transparent={true}
-        onRequestClose={handleCloseModal}
+        onRequestClose={() => setIsAddModalOpen(false)}
       >
-        <View className="flex-1 justify-center items-center bg-black/60 p-5">
-          <View className="bg-background-50 rounded-xl border-2 border-primary-300 p-5 w-full max-w-[400px]">
-            <Text className="text-xl font-bold mb-4 text-primary-900">
-              Detalles del Repuesto
-            </Text>
-            <ScrollView style={{ maxHeight: 300 }}>
-              <Text>
-                <Text className="text-lg font-bold text-primary-900">
-                  Repuesto:
-                </Text>{" "}
-                <Text className="text-primary-900">{selectedItem?.name}</Text>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{ flex: 1 }}
+        >
+          <View className="flex-1 justify-center items-center bg-black/60 p-5">
+            <View className="bg-background-50 rounded-xl border-2 border-primary-300 p-6 w-full max-w-[400px]">
+              <Text className="text-xl font-bold mb-4 text-primary-900 text-center">
+                Agregar al inventario
               </Text>
-              <Text>
-                <Text className="text-lg font-bold text-primary-900">
-                  Costo Unitario:
-                </Text>{" "}
-                <Text className="text-primary-900">
-                  ${selectedItem?.unitCost}
-                </Text>
-              </Text>
-              <Text>
-                <Text className="text-lg font-bold text-primary-900">SKU:</Text>{" "}
-                <Text className="text-primary-900">
-                  {selectedItem?.sku || "N/A"}
-                </Text>
-              </Text>
-              <Text>
-                <Text className="text-lg font-bold text-primary-900">
-                  Categoría:
-                </Text>{" "}
-                <Text className="text-primary-900">
-                  {selectedItem?.category?.name || "N/A"}
-                </Text>
-              </Text>
-              <Text>
-                <Text className="text-lg font-bold text-primary-900">
-                  Estado:
-                </Text>{" "}
-                <Text className="text-primary-900">
-                  {selectedItem?.state === "available"
-                    ? "Disponible"
-                    : "No Disponible"}
-                </Text>
-              </Text>
-            </ScrollView>
-            <Button
-              variant="outline"
-              action="secondary"
-              onPress={handleCloseModal}
-              className="mt-4 bg-primary-300 border-primary-300 rounded-full p-2"
-            >
-              <ButtonText>Cerrar</ButtonText>
-            </Button>
+
+              {/* Formulario */}
+              <ScrollView
+                className="w-full"
+                style={{ maxHeight: 300 }}
+                showsVerticalScrollIndicator={false}
+                keyboardShouldPersistTaps="handled"
+              >
+                <View className="mb-3">
+                  <Text className="text-primary-900 font-semibold mb-1">
+                    Nombre del repuesto
+                  </Text>
+                  <Input>
+                    <InputField placeholder="Ej. Pantalla iPhone 12" />
+                  </Input>
+                </View>
+
+                <View className="mb-3">
+                  <Text className="text-primary-900 font-semibold mb-1">
+                    Costo unitario
+                  </Text>
+                  <Input>
+                    <InputField
+                      placeholder="Ej. 250.00"
+                      keyboardType="numeric"
+                    />
+                  </Input>
+                </View>
+
+                <View className="mb-3">
+                  <Text className="text-primary-900 font-semibold mb-1">
+                    SKU
+                  </Text>
+                  <Input>
+                    <InputField placeholder="Ej. IP12-SCR-001" />
+                  </Input>
+                </View>
+
+                <View className="mb-3">
+                  <Text className="text-primary-900 font-semibold mb-1">
+                    Categoría
+                  </Text>
+                  <View className="border-2 border-primary-300 rounded-xl bg-background-50 overflow-hidden">
+                    <Picker
+                      selectedValue={newCategory}
+                      onValueChange={(itemValue) => setNewCategory(itemValue)}
+                      style={{
+                        fontSize: 14,
+                      }}
+                      itemStyle={{
+                        fontSize: 14,
+                      }}
+                      dropdownIconColor="#FFB74D"
+                    >
+                      <Picker.Item label="Seleccionar categoría..." value="" />
+                      <Picker.Item label="Pantallas" value="Pantallas" />
+                      <Picker.Item label="Baterias" value="Baterias" />
+                      <Picker.Item label="Conectores" value="Conectores" />
+                      <Picker.Item label="Placas Bases" value="Placas Bases" />
+                      <Picker.Item label="Cámaras" value="Cámaras" />
+                      <Picker.Item label="Micas" value="Micas" />
+                    </Picker>
+                  </View>
+                </View>
+              </ScrollView>
+
+              {/* Botones */}
+              <View className="flex-row justify-between mt-6">
+                <Button
+                  variant="outline"
+                  action="secondary"
+                  onPress={() => setIsAddModalOpen(false)}
+                  className="bg-background-200 border-primary-300 rounded-full px-5"
+                >
+                  <ButtonText>Cancelar</ButtonText>
+                </Button>
+
+                <Button
+                  className="bg-primary-400 border-primary-400 rounded-full px-5"
+                  onPress={() => setIsAddModalOpen(false)}
+                >
+                  <ButtonText>Agregar</ButtonText>
+                </Button>
+              </View>
+            </View>
           </View>
-        </View>
+        </KeyboardAvoidingView>
       </RNModal>
     </>
   );
 };
-
-export default InventoryPage;
