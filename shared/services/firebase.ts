e641e1d043b5524e66fec890843ee5097fcca855
Kimberley López <kimberley.lopezr@gmail.com>
2025-10-15 23:13:55 -0600

    Se modificaron los archivos firebase.ts y .firebaserc para modificar la conexion a la base de datos fix-track 2. Todo funciona correctamente. Kim López.

--- shared/services/firebase.ts
+++ shared/services/firebase.ts
@@ -6,12 +6,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmAoozhfJOvceM1Cywm1L8YUzKEND0Fd4",
-  authDomain: "fix-track-9e24c.firebaseapp.com",
-  projectId: "fix-track-9e24c",
-  storageBucket: "fix-track-9e24c.firebasestorage.app",
-  messagingSenderId: "632487064290",
-  appId: "1:632487064290:web:0e9bd6f2ea21395c0da4bb",
+  apiKey: "AIzaSyBUYBrlfW4ZiCarqr2_w_sQWMq-0Ykl7g8",
+  authDomain: "fix-track-2.firebaseapp.com",
+  projectId: "fix-track-2",
+  storageBucket: "fix-track-2.firebasestorage.app",
+  messagingSenderId: "453559085337",
+  appId: "1:453559085337:web:41a503f56af4f5ef57bd88",
+  measurementId: "G-BSPLKCERSH"
 };
 
 export const app = initializeApp(firebaseConfig);
